6315f7cecc9b3c947f9e821921cbd89a1ca6eed0
Julio Bitencourt <julio@contagia.com.br>
2021-11-05 17:09:09 -0300

    Create post.php

---
+++ post.php
@@ -0,0 +1,34 @@
+<?php
+
+class Post {
+
+	protected $post;
+	protected $id;
+
+	public function __construct($id)
+	{
+		$this->id = $id;
+	}
+
+	public function find($id)
+	{
+		$this->post = get_post($id);
+	}
+
+	public function __get($name)
+	{
+		return get_post_meta($this->id, $name, true);
+	}
+
+	public function __set($name, $value)
+	{
+		return update_post_meta($this->id, $name, $value);
+	}
+}
+
+$post = new SomaPost($postId);
+
+echo $post->name;
+
+$post->city = "São Paulo";
+echo $post->city); // São Paulo
